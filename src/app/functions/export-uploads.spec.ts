543c8e651dbf6bc8532d06ddfcc4b0306217793b
Borges10002 <borges10002@gmail.com>
2026-05-13 22:02:09 -0300

    export-uplods-spec

--- src/app/functions/export-uploads.spec.ts
+++ src/app/functions/export-uploads.spec.ts
@@ -1,20 +1,39 @@
 import { randomUUID } from 'node:crypto';
-import { describe, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { exportUploads } from '@/app/functions/export-uploads';
+import { isRight, unwrapEither } from '@/shared/either';
 import { makeUpload } from '@/test/factories/make-upload';
+
+vi.mock('@/infra/storage/upload-file-to-storage', () => {
+  return {
+    uploadFileToStorage: vi.fn().mockImplementation(() => {
+      const key = `downloads/${randomUUID()}.csv`;
+
+      return {
+        key,
+        url: `https://storage.com/${key}`,
+      };
+    }),
+  };
+});
 
 describe('export uploads', () => {
   it('should be able to export uploads', async () => {
     const namePattern = randomUUID();
 
-    const upload1 = await makeUpload({ name: `${namePattern}.wep` });
-    const upload2 = await makeUpload({ name: `${namePattern}.wep` });
-    const upload3 = await makeUpload({ name: `${namePattern}.wep` });
-    const upload4 = await makeUpload({ name: `${namePattern}.wep` });
-    const upload5 = await makeUpload({ name: `${namePattern}.wep` });
+    await makeUpload({ name: `${namePattern}.wep` });
+    await makeUpload({ name: `${namePattern}.wep` });
+    await makeUpload({ name: `${namePattern}.wep` });
+    await makeUpload({ name: `${namePattern}.wep` });
+    await makeUpload({ name: `${namePattern}.wep` });
 
     const sut = await exportUploads({
       searchQuery: namePattern,
     });
+
+    expect(isRight(sut)).toBe(true);
+    expect(unwrapEither(sut).reportUrl).toEqual(
+      expect.stringContaining('https://storage.com/downloads/')
+    );
   });
 });
--- src/app/functions/export-uploads.ts
+++ src/app/functions/export-uploads.ts
@@ -1,7 +1,11 @@
+import { PassThrough } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+import { stringify } from 'csv-stringify';
 import { ilike } from 'drizzle-orm';
 import { z } from 'zod';
 import { db, pg } from '@/infra/db';
 import { schema } from '@/infra/db/schemas';
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage';
 import { type Either, makeRight } from '@/shared/either';
 
 const exportUploadsInput = z.object({
@@ -34,5 +38,31 @@ export async function exportUploads(
 
   const cursor = pg.unsafe(sql, params as string[]).cursor(50);
 
-  return makeRight({ uploads, total });
+  const csv = stringify({
+    delimiter: ',',
+    header: true,
+    columns: [
+      { key: 'id', header: 'ID' },
+      { key: 'name', header: 'Name' },
+      { key: 'remote_url', header: 'URL' },
+      { key: 'created_at', header: 'Uploaded At' },
+    ],
+  });
+
+  const uploadToStorageStream = new PassThrough();
+
+  const convertToCSVPipeline = pipeline(cursor, csv, uploadToStorageStream);
+
+  const uloadToStorage = uploadFileToStorage({
+    contentType: 'text/csv',
+    folder: 'downloads',
+    fileName: `${new Date().toISOString()}-uploads.csv`,
+    contentStream: uploadToStorageStream,
+  });
+
+  const [{ url }] = await Promise.all([uloadToStorage, convertToCSVPipeline]);
+
+  console.log(url);
+
+  return makeRight({ reportUrl: url });
 }
